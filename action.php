209fb19279544a89c179af8cd61b9a164cff2247
Elsels5 <134347993+Elsels5@users.noreply.github.com>
2023-05-22 19:43:34 -0600

    Add files via upload

---
+++ action.php
@@ -0,0 +1,61 @@
+﻿<?php
+
+# ---Parametros---
+$txtInicio = $_REQUEST['txtInicio'];
+$txtFin = $_REQUEST['txtFin'];
+
+#-- Salida ----
+
+echo "<html>";
+echo "<head>";
+echo "<title>Formulario</title>";
+echo "</head>";
+echo "<body bgcolor='black' text='white'>";
+echo "<center>";
+echo "<h1>Números Primos</h1>";
+
+echo "<table border='1'>";
+$strResultado = "";
+$contador = 0;
+echo "<tr>";
+
+//--Resultados
+
+for ($n = $txtInicio; $n <= $txtFin; $n++) {
+    $thisColor = (fnEsPrimo($n)) ? 'red' : 'green';
+    echo "<td align='center' bgcolor='$thisColor'>$n</td>";
+    $contador++;
+    if ($contador == 40) {
+        echo "</tr><tr>";
+        $contador = 0;
+    }
+}
+echo "</table>";
+
+//--Botón
+echo "<br><a href='form.php'><button>OTRO</button></a>";
+
+//--Resultado
+echo substr($strResultado, 0, -2);
+
+#--Funciones -----
+function fnEsPrimo($valor)
+{
+    //variables
+    $multiplos = 0;
+
+    //Proceso
+    for ($n = 1; $n <= $valor; $n++) {
+        if ($valor % $n == 0)
+            $multiplos++;
+
+        if ($multiplos > 2)
+            break;
+    }
+    $resultado = ($multiplos == 2) ? true : false;
+
+    //Respuesta
+    return $resultado;
+}
+?>
+
---
+++ form.php
@@ -0,0 +1,46 @@
+﻿<?php
+
+#--Encabezado----
+
+echo "<html>";
+echo "<head>";
+	echo "<title>Formulario</title>";
+echo "</head>";
+echo "<body bgcolor='black' text='white'>";
+
+#-- Tabla ----
+
+echo "<h1 align='center'>Formulario</h1>";
+echo "<hr color='orange' width='40%' size='10' align='center'>";
+
+echo "<h2 align='center'>Cálculo de Numeros Primos</h2>";
+
+#-- Formulario ----
+
+echo "<form action=action.php method=post>";
+echo "<p>";
+	echo "<table border='0' width='32%' align='center'>";
+	echo "<tr>";
+   	 echo "<td>Rango de valores</td>";
+    	echo "<td><input type='text' name='txtInicio'></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+		echo "<td>Inicio</td>";
+		echo "<td><input type='text' name='txtInicio'></td>";
+	echo "</tr>";
+	echo "<tr>";
+		#--Botones--
+		echo "<td colspan='2'>";
+		echo "<input type='submit' value='ENVIAR'>";
+		echo "<input type='reset' value='CANCELAR'>";
+		echo "</td>";
+	echo "</tr>";
+	echo "</table>";
+echo "</p>";
+echo "</form>";
+
+echo "</body>";
+echo "</html>";
+
+?>
